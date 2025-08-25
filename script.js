b571895c58f295561adfc6908a4d20887624e168
andysari <andyy.sari@gmail.com>
2025-08-25 18:21:07 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-// Smooth scroll pour ancres internes
+// Navigation fluide
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
-        const target = document.querySelector(this.getAttribute('href'));
-        if(target) {
-            target.scrollIntoView({ behavior: 'smooth' });
-        }
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
 });
