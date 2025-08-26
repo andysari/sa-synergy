8edc2088d68917fde650bfa98b5c0f3f53747e19
andysari <andyy.sari@gmail.com>
2025-08-26 16:10:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 // Animation fade-in au scroll
 const faders = document.querySelectorAll(".fade-in");
 
@@ -12,7 +10,7 @@ const appearOnScroll = new IntersectionObserver(function(entries, observer) {
   entries.forEach(entry => {
     if (!entry.isIntersecting) return;
     entry.target.classList.add("visible");
-    observer.unobserve(entry.target); // une seule fois
+    observer.unobserve(entry.target);
   });
 }, appearOptions);
 
