e52067a766755537a4ac118086ddcb05230b3fec
andysari <andyy.sari@gmail.com>
2025-08-29 22:30:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       nom: "Tom",
       role: "Entrepreneur",
-      texte: "Andy est exceptionnel. Sa passion et son dévouement sont inspirants. ...",
+      texte: "Andy est exceptionnel. Sa passion et son dévouement sont inspirants. Il comprend nos défis et offre des conseils pertinents. Il écoute attentivement, pose des questions perspicaces et crée un environnement sûr. Grâce à lui, j'ai acquis une meilleure connaissance de mon potentiel et atteint mes objectifs. Sa connexion authentique et son énergie positive sont motivantes. Je le recommande vivement à tous ceux qui cherchent à toujours s’améliorer et qui souhaitent avoir des conseils dans beaucoup de domaines.",
       image: "assets/tom.jpg"
     },
     {
       nom: "Marina",
       role: "Entrepreneur",
-      texte: "Andy and I collaborated in the development of my business in Athens. ...",
+      texte: "Andy and I collaborated in the development of my business in Athens. He really helped me and I always consider his advices. 8 years later I still ask him for his opinion :) Much recommended professional and a very positive human being.",
       image: "assets/marina.jpg"
     },
     {
@@ -32,25 +32,25 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       nom: "Alexandre",
       role: "Assistant comptable",
-      texte: "Très pédagogue et sérieux. C'est un professeur très polyvalent ...",
+      texte: "Très pédagogue et sérieux. C'est un professeur très polyvalent qui s'intéresse à énormément de domaines tout en en étant passionné. Très curieux et se posant beaucoup de question, il a un sens critique affûté. Pour finir, il possède beaucoup de compétences et de connaissance à transmettre, cela est symbolisé par son nombre de certifications et de formations passées.",
       image: "assets/alexandre.jpg"
     },
     {
       nom: "Mélanie",
       role: "Étudiante en Master Finance",
-      texte: "Andy est très professionnel et réactif. ...",
+      texte: "Andy est très professionnel et réactif. Il a su m’accompagner tout au long de mon parcours scolaire post bac+4. Je le recommande également pour son implication dans les projets de secteur financier.",
       image: "assets/melanie.jpg"
     },
     {
       nom: "Leontin",
       role: "Étudiant BTS MCO",
-      texte: "Je tiens à exprimer ma profonde gratitude ...",
+      texte: "Je tiens à exprimer ma profonde gratitude envers mon formateur pour son soutien inestimable dans la matière « gestion opérationnelle ». Grâce à ses enseignements attentifs et à sa disponibilité exemplaire, j'ai pu valider mon BTS avec une note exceptionnelle de 18 en gestion opérationnelle. Son approche sympathique et sa capacité à expliquer clairement les concepts ont grandement contribué à ma réussite. Je le recommande vivement à quiconque cherche à progresser dans ce domaine, ainsi que les nombreux domaines qu’il couvre…",
       image: "assets/leontin.jpg"
     },
     {
       nom: "Nicolas",
       role: "Étudiant BTS Comptabilité et Gestion",
-      texte: "J’ai pu approfondir mes connaissances sur le bilan ...",
+      texte: "J’ai pu approfondir mes connaissances sur le bilan et le compte de résultat pour mon diplôme en comptabilité, pour ça je recommande vivement Andy.",
       image: "assets/nicolas.jpg"
     }
   ];
